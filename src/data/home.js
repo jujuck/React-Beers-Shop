8fe54ef1b8afb4735f49a5b698668b12ddd12cf0
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-08 13:29:59 +0200

    Complement du projet avec des data et images

---
+++ src/data/home.js
@@ -0,0 +1,89 @@
+const home = {
+  hero: {
+    title: "Our Beers shop and so More",
+    subTitle: "Discover all our products, with their recipe and ingredients with great joy !!!",
+    link: {
+      to: "/beers",
+      label: "Discover our most famous beers"
+    }
+  },
+  phylosophy: {
+    title: "Why do we care so much ???",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem et rerum impedit id eum dolorem ipsum reprehenderit, nesciunt aspernatur cum mollitia inventore aut distinctio, tempore consectetur adipisci fugit cupiditate laborum. Ratione ab officia quisquam, illo esse dolore sapiente soluta blanditiis, suscipit placeat, at dicta corrupti mollitia nulla deserunt rem consectetur autem! Aliquid porro expedita accusantium. Debitis nisi quae non maiores? Fugit sed, ut deleniti aliquid ipsam, vero, libero iusto sequi animi earum perspiciatis asperiores possimus distinctio labore illum incidunt aliquam tempora? Repellendus nisi officiis tempora maiores harum ea repellat nemo. Iure natus optio minus. Possimus at totam ut commodi cumque vel illo sint dignissimos, adipisci neque nostrum aliquid fugit quasi ullam? Debitis temporibus ab iste blanditiis provident suscipit, quo ex. Sapiente, quo ratione laboriosam cum assumenda possimus consequuntur ducimus nulla tempore facilis non vel nemo maiores est accusantium sunt id perspiciatis. Ad, iste nemo eum aut similique amet fugiat facilis. Praesentium assumenda odit minus doloribus reprehenderit doloremque debitis reiciendis! Aspernatur ducimus, dolorum hic fuga maxime atque vel vero aut, minima cumque ab iusto! Id culpa odit voluptatum, mollitia accusantium non? Sint, aspernatur excepturi obcaecati facilis iste, nihil saepe totam deserunt, voluptas est quibusdam itaque officia ad fuga sunt aperiam mollitia cupiditate atque! Corporis nisi excepturi repellendus vitae nulla sequi deserunt. Dolore ex eveniet dolorum exercitationem labore, numquam temporibus blanditiis tempora iusto at voluptate nostrum porro aliquam similique laborum magnam in cupiditate iste dignissimos totam commodi dolor. Recusandae quasi aliquam deleniti! Deserunt beatae architecto quos aliquid consequuntur labore maxime possimus error reprehenderit! Tempora ducimus nihil dolores culpa assumenda eum ad odio possimus placeat aliquam fugiat at, cupiditate, quis quas voluptatum tenetur! Ducimus ipsa error nihil quasi exercitationem alias, iusto recusandae, velit molestiae repellat similique odio, culpa ipsam at harum. Odio quasi quaerat porro similique nulla voluptas dolores praesentium. Iure, earum maxime!",
+    image: {
+      src: "./assets/my-beer-my-work.jpg",
+      alt: "my beer and my work, two fusional passion"
+    }
+  },
+  hops: {
+    title: "Discover our best malt",
+    imems: [
+      {
+        title: "Lager Malt",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Wheat",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Maris Otter Extra Pale",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Caramalt",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Munich",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Propino Pale Malt",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Wheat Malt",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Acidulated Malt for kettle souring",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Propino Pale Malt for kettle souring",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      },
+      {
+        title: "Dark Crystal",
+        description: "orem ipsum dolor sit amet, consectetur adipisicing elit. Quae, assumenda. Provident eius quam vitae aliquid possimus ex sapiente dignissimos voluptas placeat magni! Natus suscipit molestiae quibusdam, officia eum animi quasi! Vero nihil distinctio inventore laudantium deserunt excepturi voluptates. Accusantium soluta maxime aliquam cumque minima error accusamus fuga est voluptatem quae excepturi impedit numquam, velit officiis animi libero asperiores magni quisquam."
+      }
+    ]
+  },
+  customer: {
+    title: "What our costumers think ?",
+    items: [
+      {
+        comment: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis suscipit quibusdam numquam minima velit rem! Laboriosam praesentium possimus omnis eveniet voluptatum, ex ea. Ducimus itaque odit dolore vitae. Quisquam, nemo?",
+        avatar: {
+          src: "./assets/avatar.jpeg",
+          alt: "Bob the farmer, France, Picardie"
+        }
+      },
+      {
+        comment: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis suscipit quibusdam numquam minima velit rem! Laboriosam praesentium possimus omnis eveniet voluptatum, ex ea. Ducimus itaque odit dolore vitae. Quisquam, nemo?",
+        avatar: {
+          src: "./assets/avatar.jpeg",
+          alt: "Theo, informatic ingeneer, France, Paris"
+        }
+      },
+      {
+        comment: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis suscipit quibusdam numquam minima velit rem! Laboriosam praesentium possimus omnis eveniet voluptatum, ex ea. Ducimus itaque odit dolore vitae. Quisquam, nemo?",
+        avatar: {
+          src: "./assets/avatar.jpeg",
+          alt: "Sylvie, cryptotherapist, Belgium, Bruxelle"
+        }
+      }
+    ]
+  }
+}
