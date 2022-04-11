e225d02b90958e0c68cd50d514a25df5c2a7b4b9
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-11 12:09:30 +0200

    Part 2, step 3, ajout du composant hero avec ses props

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = ({ hero }) => {
+  return (
+    <div>
+      {console.log(hero)}
+      Hero
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 /**Import de données */
 import home from '../data/home';
 
+/** Import des composants */
+import Hero from '../components/Hero';
+
 const Home = () => {
   return (
     <div>
       {console.log(home)}
       Home
+      <Hero hero={home.hero} />
     </div>
   )
 }
