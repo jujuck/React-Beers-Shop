af673a683b0e54c727cd324b0587ccc9325a298b
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-13 16:38:03 +0200

    Part4, step 3, récupération du param, mise en place du fecth et mémorisation de la bière en state

--- src/pages/BeerDetail.jsx
+++ src/pages/BeerDetail.jsx
@@ -1,6 +1,18 @@
-import Reac from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 
 const BeerDetail = () => {
+  const { id } = useParams()
+  const [beer, setBeer] = useState({})
+
+  useEffect(() => {
+    fetch(`https://api.punkapi.com/v2/beers/${id}`)
+      .then(res => res.json())
+      .then(data => setBeer(data[0]))
+      .catch(err => console.error(err))
+  }, [id]);
+
   return (
     <div>BeerDetail</div>
   )
